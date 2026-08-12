72553c82dc058447eb8831421e485bf60bd8f739
Penguin-user17 <khanin.is.me@gmail.com>
2026-08-12 23:40:20 +0300

    Added some text style, base logic

--- src/main.c
+++ src/main.c
@@ -2,6 +2,12 @@
 #include <stdbool.h>
 
 
+const char bold_text[] = "\x1b[1m";
+const char cyan_text[] = "\x1b[36m";
+const char green_text[] = "\x1b[32m";
+const char red_text[] = "\x1b[31m";
+const char reset_text[] = "\x1b[0m";
+
 typedef struct {
     int id;
     char description[251];
@@ -10,27 +16,46 @@ typedef struct {
 
 void print_list_of_actions(void)
 {
-    printf("\nSelect an action\n");
+    printf("\n%sSelect an action%s\n\n", cyan_text, reset_text);
     printf("1 - add task\n");
     printf("2 - view the list of tasks\n");
     printf("3 - mark the task as completed\n");
+    printf("4 - delete task\n");
     printf("0 - exit\n");
-    printf("\n>>");
+    printf("%s\n>>", green_text);
 }
 
 int main(void)
 {
-    int action = -1;
-    printf("\nWelcome to your own TODO-list!\n");
+    int action;
+    printf("\n%s%sWelcome to your own TODO-list!%s\n\n", bold_text, cyan_text, reset_text);
     while (true)
     {
         print_list_of_actions();
-        if (scanf("%d", &action) != 1 || !(0 <= action && action <= 3))
+        if (scanf("%d", &action) != 1 || !(0 <= action && action <= 4))
         {
             int c;
             while ((c = getchar()) != '\n' && c != EOF);
-            printf("\nIncorrect action, try again\n\n");
+            printf("\n%sIncorrect action, try again%s\n\n", red_text, reset_text);
             continue;
         }
+        printf("%s", reset_text);
+        if (action == 1)
+        {
+
+        } else if (action == 2)
+        {
+
+        } else if (action == 3)
+        {
+
+        } else if (action == 4)
+        {
+
+        } else
+        {
+            break;
+        }
     }
+    printf("\n%s%sProgramm has finished work\n", bold_text, cyan_text);
 }
